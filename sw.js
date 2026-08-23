24ceb62b27f7527c0ab6782b68f01f59c9a1246d
Ossain-cyber <hussainadedamolalawal@gmail.com>
2026-08-23 15:48:07 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'tego-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
